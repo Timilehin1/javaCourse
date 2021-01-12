c3383389bcd6304294e7014f9e4fe993550834b2
Timilehin <timilehin@seamlesshr.com>
2021-01-12 14:39:43 +0100

    Created a score grading system

---
+++ src/IfStatements/ScoreGradingSystem.java
@@ -0,0 +1,34 @@
+package IfStatements;
+//Display a letter grade for a student based on their score
+
+import java.util.Scanner;
+
+public class ScoreGradingSystem {
+    public static void main(String args[]){
+        //Get student score
+        System.out.println("Input your score");
+        Scanner scanner = new Scanner(System.in);
+        double score = scanner.nextDouble();
+
+        //Make grading decision
+        char grade;
+        if (score < 40){
+            grade = 'E';
+        }
+        else if (score < 50){
+            grade = 'D';
+        }
+        else if (score < 60){
+            grade = 'C';
+        }
+        else if (score  < 70){
+            grade = 'B';
+        }
+        else{
+            grade = 'A';
+        }
+        System.out.println("Your grade is " + grade);
+
+        }
+    }
+
